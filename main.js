35f7ca988c167d2979c7407cbf6595b9943f42f1
Piya Kaewket <piya.pkk@outlook.com>
2021-08-15 19:35:29 +0700

    .

---
+++ main.js
@@ -0,0 +1,161 @@
+require("dotenv").config();
+const express = require("express")
+const app = express();
+const { default: axios } = require("axios");
+const Discord = require("discord.js");
+const ms = require("ms");
+var cron = require("cron");
+const client = new Discord.Client({
+  partials: ["MESSAGE"],
+});
+
+let data = [] 
+let newCase = 0
+let newRecovery = 0
+let newDeath = 0
+let totalCase = 0
+let totalRecovery = 0
+let totalDeath = 0
+let time = ''
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  axios
+  .get("https://disease.sh/v3/covid-19/countries/thailand")
+  .then((res) => { 
+      data = res.data 
+      console.log("Success!")
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+});
+
+let scheduledMessage = new cron.CronJob('00 00 16 * * *', () => {
+let channel = client.channels.cache.get('876310958191042590');
+// let channel = client.channels.cache.get('790578744166973461');
+    axios
+      .get("https://disease.sh/v3/covid-19/countries/thailand")
+      .then((res) => {
+        data = res.data
+        newCase = data.todayCases;
+        newRecovery = data.todayRecovered
+        newDeath = data.todayDeaths
+        totalCase = data.cases
+        totalRecovery = data.recovered
+        totalDeath = data.deaths
+        time = data.updated
+        let date = new Date().toLocaleDateString('th-TH');
+        channel.send({
+            embed: {
+                title: `:loudspeaker:    รายงานยอดผู้ติดเชื้อประจำวันที่ ${date}`,
+                color: 16514839,
+                fields: [
+                  {
+                    name: `:thermometer_face:   ยอดผู้ติดเชื้อวันนี้ `,
+                    value: `>>>    ${newCase}    คน`,
+                    inline: true
+                  },
+                  {
+                    name: `||   :muscle:   หายป่วยวันนี้ `,
+                    value: `>>>    ${newRecovery}    คน`,
+                    inline: true
+                  },
+                  {
+                    name: `||   :skull_crossbones:   เสียชีวิตวันนี้ `,
+                    value: `>>>    ${newDeath}    คน`,
+                    inline: true
+                  },
+                  {
+                      name: `🦠   ยอดติดเชื้อรวม `,
+                      value: `>>>    ${totalCase}    คน`,
+                      inline: true
+                  },
+                  {
+                      name: `||   💉   รักษาหายแล้วทั้งหมด `,
+                      value: `>>>    ${totalRecovery}    คน`,
+                      inline: true
+                  },
+                  {
+                      name: `||   :skull_crossbones:   เสียชีวิตรวม `,
+                      value: `>>>   ${totalDeath}    คน`,
+                      inline: true
+                  },
+                  {
+                    name: `Ref: https://disease.sh/`,
+                    value: `ปล.api ตัวใหม่ไม่แน่ใจว่าอัพเดทข้อมูลตอนกี่โมง`,
+                  },
+                ],
+              },
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
+  scheduledMessage.start()
+
+client.on("message", (msg) => {
+  if (msg.content === "!covid") {
+    if(msg.channel.id == "876310958191042590"){
+    // if(msg.channel.id == "790578744166973461"){
+      newCase = data.todayCases;
+        newRecovery = data.todayRecovered
+        newDeath = data.todayDeaths
+        totalCase = data.cases
+        totalRecovery = data.recovered
+        totalDeath = data.deaths
+        time = data.updated
+        let date = new Date().toLocaleDateString('th-TH');
+        
+        msg.channel.send({
+            embed: {
+              title: `:loudspeaker:    รายงานยอดผู้ติดเชื้อประจำวันที่ ${date}`,
+              color: 16514839,
+              fields: [
+                {
+                  name: `:thermometer_face:   ยอดผู้ติดเชื้อวันนี้ `,
+                  value: `>>>    ${newCase}    คน`,
+                  inline: true
+                },
+                {
+                  name: `||   :muscle:   หายป่วยวันนี้ `,
+                  value: `>>>    ${newRecovery}    คน`,
+                  inline: true
+                },
+                {
+                  name: `||   :skull_crossbones:   เสียชีวิตวันนี้ `,
+                  value: `>>>    ${newDeath}    คน`,
+                  inline: true
+                },
+                {
+                    name: `🦠   ยอดติดเชื้อรวม `,
+                    value: `>>>    ${totalCase}    คน`,
+                    inline: true
+                },
+                {
+                    name: `||   💉   รักษาหายแล้วทั้งหมด `,
+                    value: `>>>    ${totalRecovery}    คน`,
+                    inline: true
+                },
+                {
+                    name: `||   :skull_crossbones:   เสียชีวิตรวม `,
+                    value: `>>>   ${totalDeath}    คน`,
+                    inline: true
+                },
+                {
+                  name: `Ref: https://disease.sh/`,
+                  value: `ปล.api ตัวใหม่ไม่แน่ใจว่าอัพเดทข้อมูลกี่โมง`,
+                },
+              ],
+            }
+        });
+    }
+        
+  }
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
+client.login(process.env.BOT_TOKEN);
